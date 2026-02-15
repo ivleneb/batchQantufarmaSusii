18b926a02030d95dd1b688860cc7e33f2576f9a8
Ivleneb <beenelvi.godoy@gmail.com>
2026-02-15 17:23:28 -0500

    factorizar proxVencimiento

--- proxVencimiento/proxVencimiento.py
+++ proxVencimiento/proxVencimiento.py
@@ -1,61 +1,13 @@
 import sys
 sys.path.append(r'../')
-from lib.QantuMedicine import QantuMedicine
-from lib.QantuGalenico import QantuGalenico
-from lib.QantuDevice import QantuDevice
-from lib.QantuGeneral import QantuGeneral
-#from lib.QantuPackage import QantuPackage
-#from lib.QantuProduct import QantuProduct
-from lib.ReportDownloader import ReportDownloader
+from lib.QantuProduct import QantuProduct
+from lib.QantuConfiguration import QantuConfiguration
+from lib.SusiiProductLoader import SusiiProductLoader
 import pandas
 from datetime import datetime
-import json
 
-# Read JSON file
-with open('../lib/cfg.json', 'r', encoding='utf-8') as file:
-    dataCfg = json.load(file)
-    business_ = dataCfg["businessId"]
-
-def getProduct(prod_df, code):
-    sub_df = prod_df.loc[prod_df['CÓDIGO'] == code]
-    if len(sub_df)==1:
-        row = sub_df.iloc[0]
-        prod = None
-        if row['CATEGORÍA']=='MEDICAMENTOS':
-            prod = QantuMedicine(row['CÓDIGO'], row['NOMBRE'], row['CANTIDAD'],
-                                 row['disable (EXTRA)'], row['creado (EXTRA)'], row['STOCK MÍNIMO'])
-            #print("generic value "+row['NOMBRE'])
-            #print(row['generico (EXTRA)'])
-            #print(type(row['generico (EXTRA)']))
-            prod.setGenerico(row['generico (EXTRA)'])
-            #print(prod.getName())
-            #print(prod.isGenerico())
-        elif row['CATEGORÍA']=='GALENICOS':
-            prod = QantuGalenico(row['CÓDIGO'], row['NOMBRE'], row['CANTIDAD'],
-                                 row['disable (EXTRA)'], row['creado (EXTRA)'], row['STOCK MÍNIMO'])
-        elif row['CATEGORÍA']=='DISPOSITIVOS MEDICOS':
-            prod = QantuDevice(row['CÓDIGO'], row['NOMBRE'], row['CANTIDAD'],
-                               row['disable (EXTRA)'], row['creado (EXTRA)'], row['STOCK MÍNIMO'])
-        else:
-            prod = QantuGeneral(row['CÓDIGO'], row['NOMBRE'], row['CANTIDAD'],
-                                row['disable (EXTRA)'], row['CATEGORÍA'], row['creado (EXTRA)'],
-                                row['STOCK MÍNIMO'])
-        # add data
-        prod.setLastCost(row['PRECIO DE COMPRA'])
-        prod.setPrice(row['PRECIO DE VENTA'])
-        if isinstance(row['fecha_vto (EXTRA)'], str):
-            prod.setFechaVto(row['fecha_vto (EXTRA)'])
-        elif prod.getStock()!=0:
-            print("Invalid fecha_vto["+str(row['fecha_vto (EXTRA)'])+"] for "+prod.getName())
-            return None
-        else:
-            return None
-        return prod
-    elif len(sub_df)==0:
-        print("Product not found")
-        return None
-    else:
-        raise Exception("Code with multiple products.")
+config = QantuConfiguration()
+business_ = config.business_
 
 def createDataList(prodDict):
     data = []
@@ -71,36 +23,13 @@ def createDataList(prodDict):
 
 
 def run():
-    prodDict = {}
-    today = datetime.now().strftime("%Y-%m-%d")
+    loader = SusiiProductLoader(business_)
 
-    # download product data
-    repHeaders = ["CÓDIGO", "NOMBRE", "STOCK MÍNIMO", "CANTIDAD", "disable (EXTRA)",
-                  "creado (EXTRA)", "fecha_vto (EXTRA)", "CATEGORÍA", "PRECIO DE VENTA", "PRECIO DE COMPRA",
-                  "generico (EXTRA)"]
-    rd = ReportDownloader("Exportar productos.xlsx", "export_products",
-                          repHeaders, '2024-02-12',
-                          today)
-    file_prod = rd.execute()
-    if file_prod == "":
-        sys.exit("Can't dowloand file[Exportar productos.xlsx]")
-
-    prod_df = pandas.read_excel(file_prod, skiprows=4)
-    print("REG SIZE (prod):"+str(len(prod_df)))
-
-    # Load products
-    for index, row in prod_df.iterrows():
-        # only add sales that are products
-        prod = getProduct(prod_df, row['CÓDIGO'])
-        if prod is not None:
-            # add product to data dict
-            if prod.getCode() in prodDict:
-                raise Exception("Key must be unique")
-            prodDict[prod.getCode()]=prod
-            #if prod.getCategory()=='MEDICAMENTOS':
-            #    print(prod.getName())
-            #    print(prod.isGenerico())
-
+    # load products from q1
+    prodDict:dict[str, QantuProduct] = loader.downloadProducts()
+    if not prodDict:
+        print("Fail to downloadProducts.")
+        sys.exit(2)
 
     medsDict = {}
     otherDict = {}
