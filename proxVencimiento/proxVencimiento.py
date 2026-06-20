9ffd6be5078b2bd4bb32e478f9685ccd25972bef
Ivleneb <beenelvi.godoy@gmail.com>
2026-06-20 16:18:54 -0500

    Include disabled products

--- proxVencimiento/proxVencimiento.py
+++ proxVencimiento/proxVencimiento.py
@@ -5,6 +5,7 @@ from lib.QantuConfiguration import QantuConfiguration
 from lib.SusiiProductLoader import SusiiProductLoader
 import pandas
 from datetime import datetime
+from lib.BatchUtils import BatchUtils
 
 config = QantuConfiguration()
 business_ = config.business_
@@ -18,7 +19,7 @@ def createDataList(prodDict):
 
         if prod.getRemainingDays()<=60 and prod.getStock()!=0:
             data.append( [prod.getCode(), prod.getName(), prod.getCategory(), prod.getLastCost(),
-                      prod.getPrice(), prod.getStock(), prod.getCreatedAt(), prod.getFechaVto()])
+                      prod.getPrice(), prod.getStock(), prod.getCreatedAt(), prod.getFechaVto(), prod.getStock()*prod.getLastCost()])
     return data
 
 
@@ -26,7 +27,7 @@ def run():
     loader = SusiiProductLoader(business_)
 
     # load products from q1
-    prodDict:dict[str, QantuProduct] = loader.downloadProducts()
+    prodDict:dict[str, QantuProduct] = loader.downloadProducts(includeDisable=True)
     if not prodDict:
         print("Fail to downloadProducts.")
         sys.exit(2)
@@ -43,15 +44,18 @@ def run():
     dataOther = createDataList(otherDict)
 
     cols = ['COD', 'NOMBRE', 'CATEGORIA', 'COSTO', 'PRECIO', 'STOCK', 
-            'CREADO', 'FECHA VTO' ]
+            'CREADO', 'FECHA VTO', 'VAL' ]
 
     outMed_df = pandas.DataFrame(dataMeds, columns = cols)
     outOther_df = pandas.DataFrame(dataOther, columns = cols)
 
     now = datetime.now().strftime("%Y%m%d")
     excel_name = str(business_)+'_ProxVto_'+now+'.xlsx'
-
-    with pandas.ExcelWriter(excel_name) as excel_writer:
+    out_path = './out'
+    BatchUtils.crear_carpeta_si_no_existe(out_path)
+    fullpath = out_path+'/'+excel_name
+    
+    with pandas.ExcelWriter(fullpath) as excel_writer:
         outMed_df.to_excel(excel_writer, sheet_name='Medicamentos', index=False)
         outOther_df.to_excel(excel_writer, sheet_name='Otros', index=False)
 
